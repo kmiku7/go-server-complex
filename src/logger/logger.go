4e2f7e4213f46352cbe81232d8a83d7222a844df
kmiku7 <kakoimiku@gmail.com>
2019-07-31 10:14:11 +0800

    Update logger.go

--- src/logger/logger.go
+++ src/logger/logger.go
@@ -25,19 +25,6 @@ type Logger interface {
 	Fatal(args ...interface{})
 }
 
-//import "github.com/kmiku7/golog"
-//
-//func main() {
-//  fileBackend, err := golog.NewFileBackend("./log")
-//  if err != nil {
-//    panic(err)
-//  }
-//  defer fileBackend.Close()
-//  fileBackend.SetRotateFile(true, 24)
-//
-//  fileBackend.Log(golog.Info, "Hello World!")
-//}
-
 type fileBackendWrapper struct {
 	fileBackend *golog.FileBackend
 }
